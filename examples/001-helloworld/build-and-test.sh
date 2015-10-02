f6cd6d0f44f0ae92cb9bfc3ed8b80f62b2759e0e
Tamas Kenez <tamaskenez@users.noreply.github.com>
2015-10-02 10:08:51 +0200

    Update build-and-test.sh

--- examples/001-helloworld/build-and-test.sh
+++ examples/001-helloworld/build-and-test.sh
@@ -5,11 +5,11 @@ mkdir -p build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PWD/../out \
     -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTING=ON \
     ..
-cmake --build .
+cmake --build . --config Debug
 ctest
 
 # test Release build and installation
 cmake -DCMAKE_BUILD_TYPE=Release .
-cmake --build . --target install
+cmake --build . --target install --config Release
 ctest
 
